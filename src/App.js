c7a72792a7d3f7a429719ecd519279720f6ee5d7
muneneevans <munene.evansk@gmail.com>
2018-09-27 15:01:51 +0300

    [UPDATE] set up react-dip; great but i unsupported and unpopular

--- src/App.js
+++ src/App.js
@@ -1,21 +1,44 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from "react";
+import Dip from "react-dip";
 
-class App extends Component {
+function Component1() {
+  return (
+    <Dip dipId="quickStart" style={{ background: "red", height: "200px" }}>
+      <div style={{ background: "gray", height: "50px" }}>some content</div>
+      some other content <br />
+    </Dip>
+  );
+}
+
+function Component2() {
+  return (
+    <Dip
+      dipId="quickStart"
+      style={{ position: "absolute", top: "100px", background: "green" }}
+    >
+      <div style={{ background: "gray", height: "50px" }}>some content</div>
+      some other content <br />
+      etc...
+    </Dip>
+  );
+}
+
+// use complex state here
+// or a routing solution such as react-router
+// or connect it to redux, or ustated
+export default class MyStatefulParent extends Component {
+  state = { currentStep: 0 };
+  toggleState = () =>
+    this.setState(state => ({
+      currentStep: (state.currentStep + 1) % 2
+    }));
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
-      </div>
+      <section>
+        <h1> Quick Start example </h1>
+        <button onClick={this.toggleState}>toggle me</button>
+        {this.state.currentStep === 0 ? <Component1 /> : <Component2 />}
+      </section>
     );
   }
 }
-
-export default App;
